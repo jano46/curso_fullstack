8b52af36e34822f7ab130c6bc8a2f2962f5d34f8
Alejandro Becerra Ramírez <alejandro.becerra1239@alumnos.udg.mx>
2025-08-18 20:04:09 -0600

    new proyect
    se agregó nuevo proyecto para calcular intereses

---
+++ Calculadora/assets/scripts/script.js
@@ -0,0 +1,25 @@
+/*Función para validar costos y imprimir los calculos */
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value; 
+    var interest = principal * years * rate / 100;
+    var amount = parseInt(principal) + parseFloat(interest);
+    var result = document.getElementById("result");
+    var year = new Date().getFullYear() + parseInt(years);
+    p = document.getElementById("principal").value;
+
+    if (principal <= 0) {
+        alert('Please enter a positive number!');
+        document.getElementById("principal").focus();
+    }else {
+        result.innerHTML = "If you deposit $" + "<mark>" + principal + "</mark>" + ",\<br\> at an interest rate of " + "<mark>" + rate + "%" + "</mark>" + "\<br\> You will receive an amount of $" + "<mark>" + amount + "</mark>" + ",\<br\> in the year " + "<mark>" + year + "</mark>" + "\<br\>";
+    }
+}
+
+/*Función para actualizar el valor del rate */
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
